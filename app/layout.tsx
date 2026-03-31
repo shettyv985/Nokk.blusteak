d4bcd8bbe79df8a36c9cbaad50705c4f1e772980
shettyv985 <shettyv2026@gmail.com>
2026-03-31 10:34:30 +0530

    testajin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,24 +25,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <meta name="google-site-verification" content="4mwLHBEPlzRDJz0M-2oLHu005xkdb3T-qiUOw6K0hRI" />
-    {/* <!-- Google Tag Manager --> */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <head>
+        <meta name="google-site-verification" content="4mwLHBEPlzRDJz0M-2oLHu005xkdb3T-qiUOw6K0hRI" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WJG7H6DX');</script>
-{/* <!-- End Google Tag Manager --> */}
-    </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+})(window,document,'script','dataLayer','GTM-WJG7H6DX');`}
+        </Script>
+
         {children}
-        {/* <!-- Google Tag Manager (noscript) --> */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJG7H6DX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-{/* <!-- End Google Tag Manager (noscript) --> */}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJG7H6DX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </body>
     </html>
   );
