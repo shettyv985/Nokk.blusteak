08ea56a7ca9da49a462a434b1d76636420fb47be
shettyv985 <shettyv2026@gmail.com>
2026-03-31 10:32:18 +0530

    testajin

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    dsffd
+    <head>
+      <meta name="google-site-verification" content="4mwLHBEPlzRDJz0M-2oLHu005xkdb3T-qiUOw6K0hRI" />
+    {/* <!-- Google Tag Manager --> */}
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WJG7H6DX');</script>
+{/* <!-- End Google Tag Manager --> */}
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJG7H6DX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
     </html>
   );
